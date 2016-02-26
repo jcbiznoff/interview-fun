e7b6457946c80653e20de38b56e3aa5af0729a3e
tonytangandroid <tonytangandroid@gmail.com>
2016-02-26 17:09:32 -0500

    tony's code

--- app/src/main/java/com/clayons/interviewquestions/DetailActivity.java
+++ app/src/main/java/com/clayons/interviewquestions/DetailActivity.java
@@ -2,13 +2,70 @@ package com.clayons.interviewquestions;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.clayons.interviewquestions.Model.Person;
+import com.squareup.picasso.Picasso;
 
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean initialStatus;
+    private Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
+
+        if (getIntent() == null || getIntent().getParcelableExtra(ContactFragment.KEY_PERSON_DETAIL) == null) {
+            throw new RuntimeException("You must pass the person object");
+        }
+
+        person = getIntent().getParcelableExtra(ContactFragment.KEY_PERSON_DETAIL);
+
+        ImageView ivAvatar = (ImageView) findViewById(R.id.ivAvatar);
+        TextView tvFristName = (TextView) findViewById(R.id.tvFristName);
+        TextView tvLastName = (TextView) findViewById(R.id.tvLastName);
+        TextView tvAge = (TextView) findViewById(R.id.tvAge);
+        TextView tvPhoneNum = (TextView) findViewById(R.id.tvPhoneNum);
+
+        Picasso.with(this).load(person.getPhotoUrl()).into(ivAvatar);
+        tvFristName.setText(person.getFirstName());
+        tvLastName.setText(person.getLastName());
+        tvAge.setText(person.getAge());
+        tvPhoneNum.setText(person.getPhoneNum());
+
+        Button buttonLike = (Button) findViewById(R.id.btnLike);
+        initialStatus = person.isFavorited();
+        buttonLike.setText(initialStatus ? getString(R.string.unlike) : getString(R.string.like));
+        buttonLike.setOnClickListener(this);
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+        person.setFavorited(!person.isFavorited());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (person.isFavorited() != initialStatus) {
+            setResult(RESULT_OK);
+            finish();
+        } else {
+            super.onBackPressed();
+        }
+
     }
 }
--- app/src/main/java/com/clayons/interviewquestions/Model/Person.java
+++ app/src/main/java/com/clayons/interviewquestions/Model/Person.java
@@ -1,14 +1,27 @@
 package com.clayons.interviewquestions.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by jaychung on 11/16/15.
  */
-public class Person {
+public class Person implements Parcelable {
+    public static final Creator<Person> CREATOR = new Creator<Person>() {
+        public Person createFromParcel(Parcel source) {
+            return new Person(source);
+        }
+
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
     private String firstName;
     private String lastName;
     private Integer age;
     private String phoneNum;
     private String photoUrl;
+    private boolean favorited;
 
     public Person(String firstName, String lastName, Integer age, String phoneNum, String photoUrl) {
         this.firstName = firstName;
@@ -20,5 +33,77 @@ public class Person {
 
     public Person() {
 
+    }
+
+    protected Person(Parcel in) {
+        this.firstName = in.readString();
+        this.lastName = in.readString();
+        this.age = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.phoneNum = in.readString();
+        this.photoUrl = in.readString();
+        this.favorited = in.readByte() != 0;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.firstName);
+        dest.writeString(this.lastName);
+        dest.writeValue(this.age);
+        dest.writeString(this.phoneNum);
+        dest.writeString(this.photoUrl);
+        dest.writeByte(favorited ? (byte) 1 : (byte) 0);
     }
 }
